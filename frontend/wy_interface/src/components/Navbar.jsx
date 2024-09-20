89b6e2aae0a4d1433ef90bd9460000fc0ce31bfe
Frank Francione <frankiefrancione@gmail.com>
2024-09-20 14:46:08 -0500

    Added simple hamburger menu

--- frontend/wy_interface/src/components/Navbar.jsx
+++ frontend/wy_interface/src/components/Navbar.jsx
@@ -5,38 +5,7 @@ import { NavLink } from "react-router-dom";
 const Menu = () => {
   return (
     <>
-      <div className="flex flex-row justify-between py-4 px-6">
-        <NavLink to="/">
-          <h1 className="orbitron">Weyland-Yutani Corporation</h1>
-        </NavLink>
-        <ul className="flex flex-row gap-5 orbitron">
-          <li className="hover:text-yellow-200">
-            <NavLink to="/home/chat">Chat Logs</NavLink>
-          </li>
-          <li className="hover:text-yellow-200">
-            <NavLink to="/home/buy">Buy</NavLink>
-          </li>
-          <li className="hover:text-yellow-200">
-            <NavLink to="/home/alien">Aliens</NavLink>
-          </li>
-          <li className="hover:text-yellow-200">
-            <NavLink to="/home/profile">Profile</NavLink>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
-};
-
-const SmallMenu = () => {
-  return (
-    <div className="flex flex-row justify-between py-4 px-6">
-      <NavLink to="/">
-        <h1 className="orbitron text-balance text-start">
-          Weyland-Yutani Corporation
-        </h1>
-      </NavLink>
-      <ul className="orbitron">
+      <ul className="flex flex-row gap-5 orbitron">
         <li className="hover:text-yellow-200">
           <NavLink to="/home/chat">Chat Logs</NavLink>
         </li>
@@ -50,7 +19,26 @@ const SmallMenu = () => {
           <NavLink to="/home/profile">Profile</NavLink>
         </li>
       </ul>
-    </div>
+    </>
+  );
+};
+
+const SmallMenu = () => {
+  return (
+    <ul className="orbitron flex flex-col gap-2 mt-2 text-right mr-2">
+      <li className="hover:text-yellow-200">
+        <NavLink to="/home/chat">Chat Logs</NavLink>
+      </li>
+      <li className="hover:text-yellow-200">
+        <NavLink to="/home/buy">Buy</NavLink>
+      </li>
+      <li className="hover:text-yellow-200">
+        <NavLink to="/home/alien">Aliens</NavLink>
+      </li>
+      <li className="hover:text-yellow-200">
+        <NavLink to="/home/profile">Profile</NavLink>
+      </li>
+    </ul>
   );
 };
 
@@ -60,12 +48,57 @@ const Navbar = () => {
   return (
     <div>
       <div>
-        <div className="hidden md:block">
-          <Menu />
-        </div>
+        <div className="flex flex-row justify-between  py-4 px-6">
+          <NavLink to="/">
+            <h1 className="orbitron text-balance text-start">
+              Weyland-Yutani Corporation
+            </h1>
+          </NavLink>
+          <div className="md:hidden">
+            <button
+              className=" rounded-md outline-none flex items-end"
+              onClick={() => setNavbar(!navbar)}
+            >
+              {navbar ? (
+                <div className="flex flex-col items-end">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 h-8 bg-opacity-55 transition-all duration-100 "
+                    viewBox="0 0 20 20"
+                    // fill={"currentColor"}
+                    stroke="white"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
 
-        <div className="md:hidden">
-          <SmallMenu />
+                  <SmallMenu />
+                </div>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8  bg-opacity-55  transition-all duration-100 "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+
+          <div className="hidden md:block">
+            <Menu />
+          </div>
         </div>
       </div>
     </div>
