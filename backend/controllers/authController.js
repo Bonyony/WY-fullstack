39f71b5f8a3459d8ad1bcb1ffe691bae87f09f9d
Frank Francione <frankiefrancione@gmail.com>
2024-09-25 07:59:46 -0500

    continued auth implementation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -67,3 +67,61 @@ exports.signup = (req, res) => {
     }
   });
 };
+
+exports.login = (req, res) => {
+  Profile.findOne({
+    username: req.body.username,
+  })
+    .populate("roles", "-__v")
+    .exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      if (!user) {
+        return res
+          .status(404)
+          .send({ message: "User not found in our system." });
+      }
+
+      let passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (!passwordIsValid) {
+        return res.status(401).send({ message: "Invalid Password" });
+      }
+
+      const token = jwt.sign({ id: user.id }, config.secret, {
+        algorithm: "HS256",
+        allowInsecureKeySizes: true,
+        expiresIn: 86400,
+      });
+
+      let authorities = [];
+
+      for (let i = 0; i < user.roles.length; i++) {
+        authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
+      }
+
+      req.session.token = token;
+
+      res.status(200).send({
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        roles: authorities,
+      });
+    });
+};
+
+exports.signout = async (req, res) => {
+  try {
+    req.session = null;
+    return res.status(200).send({ message: "You have been signed out." });
+  } catch (error) {
+    this.next(error);
+  }
+};
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,19 @@
+const { verifySignUp } = require("../middleware");
+const controller = require("../controllers/authController");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
+  });
+
+  //   make sure that the frontend routes are the same
+  app.post(
+    "/signup",
+    [verifySignUp.checkDuplicates, verifySignUp.checkRoles],
+    controller.signup
+  );
+  app.post("/login", controller.login);
+  //   needs to be made 9/25
+  app.post("/logout", controller.signout);
+};
