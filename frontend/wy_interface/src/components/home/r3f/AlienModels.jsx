32c2600f8eb5dc8f037ad65475f8602ce926e267
Frank Francione <frankiefrancione@gmail.com>
2024-12-13 18:35:38 -0600

    added base models for 3d

--- frontend/wy_interface/src/components/home/r3f/AlienModels.jsx
+++ frontend/wy_interface/src/components/home/r3f/AlienModels.jsx
@@ -1,9 +1,9 @@
 import * as THREE from "three";
 import React, { useRef, useState, Suspense } from "react";
 import { Canvas, useThree, useFrame } from "@react-three/fiber";
-import { useGLTF, Environment } from "@react-three/drei";
+import { useGLTF, OrbitControls } from "@react-three/drei";
 
-const GreyAlien = () => {
+const GreyAlien = ({ position }) => {
   const { nodes, materials } = useGLTF("/models/Alien.glb");
   console.log(nodes, materials);
   return (
@@ -11,8 +11,37 @@ const GreyAlien = () => {
       <mesh
         geometry={nodes?.Alien_mesh?.geometry}
         material={materials?.lambert2SG}
+        position={position}
       />
     </>
+  );
+};
+
+const Bunny = ({ position }) => {
+  const { nodes, materials } = useGLTF("/models/Rabbit.glb");
+  console.log(nodes, materials);
+  return (
+    <>
+      <mesh
+        geometry={nodes?.Geo_Rabbit?.geometry}
+        material={materials?.lambert2SG}
+        scale={0.05}
+        position={position}
+      />
+    </>
+  );
+};
+
+const FlyingSaucer = ({ position }) => {
+  const { nodes, materials } = useGLTF("/models/Flying saucer.glb");
+  console.log(nodes, materials);
+  return (
+    <mesh
+      geometry={nodes?.Flying_Saucer?.geometry}
+      material={materials?.Mat}
+      scale={0.2}
+      position={position}
+    />
   );
 };
 
@@ -22,8 +51,12 @@ const AlienModels = () => {
       <Suspense>
         <directionalLight position={[2, 10, 2]} intensity={0.8} />
         <ambientLight intensity={0.7} />
+        <OrbitControls enableZoom={false} makeDefault={true} />
         {/* <Environment preset="sunset" background /> */}
-        <GreyAlien />
+        <GreyAlien position={[7, 0, 0]} />
+        <FlyingSaucer position={[-7, 0, 0]} />
+        {/* y value of 1.5 will make Bunny even with the other models */}
+        <Bunny position={[0, 1.5, 0]} />
       </Suspense>
     </Canvas>
   );
